c10b89b38fa2657d9b9b36a6c37008f10acff4c4
Koushikm8926 <darkdeavil004@gmail.com>
2025-03-23 21:00:50 +0100

    Qr Code sheemer effect implemented

--- src/components/Home/HomeHeader.tsx
+++ src/components/Home/HomeHeader.tsx
@@ -1,34 +1,37 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View,  TouchableOpacity, Image } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native';
 import { commonStyles } from '../../styles/commonStyles';
 import { homeHeaderStyles } from '../../styles/homeHeaderStyles';
 import Icon from '../Global/Icon';
 import Svg,{Path, Defs,LinearGradient, Stop  } from 'react-native-svg';
-import { screenHeight, screenWidth, svgPath } from '../../utils/Constants';
+import { screenHeight,  svgPath } from '../../utils/Constants';
 import { Dimensions } from 'react-native';
 import QRGenerateModal from '../modals/QRGenerateModal';
 
 const HomeHeader = () => {
-     const [isVisible , setVisible]= useState(false);
+
+     const [isVisible , setVisible] = useState(false);
      const { width: screenWidth } = Dimensions.get('window');
+
   return (
     <View style={homeHeaderStyles.mainContainer}>
         <SafeAreaView/>
         <View style={[commonStyles.flexRowBetween, homeHeaderStyles.container]}>
+          
             <TouchableOpacity>
-                <Icon iconFamaly='Ionicons' name='menu' size={22} color='#fff'/>
+                <Icon iconFamaly="Ionicons" name='menu' size={22} color='#fff'/>
             </TouchableOpacity>
-           
-            <Image style={homeHeaderStyles.logo} source={require('../../assets/images/AppIcon.png')}
-        />
-            <TouchableOpacity>
-            <Image source={require('../../assets/images/profile.jpg')} style={homeHeaderStyles.profile}/>
-            </TouchableOpacity>
-        </View>
-      
-      <Svg height={screenHeight*0.20} width={screenWidth + 2} viewBox='0 0 1440 20' style={homeHeaderStyles.curve}>
 
+            <Image style={homeHeaderStyles.logo} source={require('../../assets/images/AppIcon.png')}/>
+     
+                <TouchableOpacity onPress={()=>setVisible(true)} style={{ backgroundColor: "red", padding: 10 }} >
+            <Image source={require('../../assets/images/profile.jpg')} style={homeHeaderStyles.profile}/>
+            </TouchableOpacity>           
+        </View>
+
+
+      <Svg height={screenHeight*0.20} width={screenWidth + 2} viewBox='0 0 1440 20' style={homeHeaderStyles.curve} pointerEvents="none">
             <Defs>
                 <LinearGradient id='grad' x1="0" y1="0" x2="0" y2='1' >
                     <Stop offset="0%" stopColor="#007AFF" stopOpacity="1" />
@@ -38,7 +41,7 @@ const HomeHeader = () => {
             <Path fill="#80BFFF" d={svgPath}/>
             <Path fill="url(#grad)" d={svgPath}/>
       </Svg>
-      {isVisible && <QRGenerateModal/> }
+      {isVisible && <QRGenerateModal onClose={()=>setVisible(false)} visible={isVisible}/>  }
     </View>
   )
 }
--- src/components/modals/QRGenerateModal.tsx
+++ src/components/modals/QRGenerateModal.tsx
@@ -1,12 +1,73 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Modal, } from 'react-native'
+import React, { FC, useEffect, useState } from 'react'
+import { modalStyles } from '../../styles/modalStyles';
+import Animated, { Easing, useSharedValue, useAnimatedStyle, withRepeat, withTiming } from 'react-native-reanimated';
 
-const QRGenerateModal = () => {
+import LinearGradient from 'react-native-linear-gradient';
+import QRCode from 'react-native-qrcode-svg'; 
+import { multiColor } from '../../utils/Constants';
+
+ 
+interface ModalProps {
+  visible:boolean;
+  onClose:()=>void;
+}
+
+
+const QRGenerateModal:FC <ModalProps> = ({visible , onClose}) => {
+  const [loading, setLoading ]= useState(false);
+  const [qrvalue, setQRValue] = useState('')
+  const shimmerTranslateX= useSharedValue(-300)
+
+  const shimmerStyle = useAnimatedStyle(()=>({
+    transform:[{translateX:shimmerTranslateX.value}]
+  }))
+
+  useEffect(()=>{
+    shimmerTranslateX.value= withRepeat(
+      withTiming(300,{duration:1500,easing:Easing.linear}),
+      -1, false,
+    )
+  },[visible])
+
+
+
+
   return (
-    <View>
-      <Text>QRGenerateModal</Text>
-    </View>
+    <Modal animationType='slide' visible={visible} presentationStyle='formSheet' onRequestClose={onClose} onDismiss={onClose}  >
+     <View style={modalStyles.modalContainer}>
+      <View style={modalStyles.qrContainer}>
+          {
+             loading || qrvalue ===null || qrvalue == '' ? (
+              <View style={modalStyles.skeleton}>
+                <Animated.View style={[modalStyles.shimmerOverlay,shimmerStyle]}>
+                  <LinearGradient
+                  colors={['#f3f3f3','#fff','#f3f3f3']}
+                  start={{x:0, y:0}}
+                  end={{x:1, y:0}}
+                  style={modalStyles.shimmerGradient}
+                  />
+                </Animated.View>
+              </View>
+             ):(
+              <QRCode 
+              value={qrvalue} 
+              size={250}
+              logoSize={60}
+              logoBackgroundColor='#fff'
+              logoMargin={2}
+              logoBorderRadius={10}
+              logo={require('../../assets/images/AppIcon.png')}
+              linearGradient={multiColor}
+              enableLinearGradient 
+              />
+             )
+          }
+      </View>
+      </View>
+     
+    </Modal>
   )
 }
 
-export default QRGenerateModal
+export default QRGenerateModal;
