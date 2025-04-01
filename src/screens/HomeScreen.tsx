8e68fb20529306cd07dd9d8be5fb652ce58a1851
Koushikm8926 <darkdeavil004@gmail.com>
2025-04-01 11:44:38 +0200

    SendReceiveButton, options and misc component added

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,21 @@
-import { View, Text } from 'react-native'
-import React, { FC, useState } from 'react'
+import { ScrollView, View, } from 'react-native'
+import React, { FC,  } from 'react'
 import { commonStyles } from '../styles/commonStyles';
 import HomeHeader from '../components/Home/HomeHeader';
+import Misc from '../components/Home/Misc';
+import Options from '../components/Home/Options';
+import SendReceiveButton from './SendReceiveButton';
 
 const  HomeScreen:FC = () => {
  
   return (
     <View style={commonStyles.baseContainer}>
       <HomeHeader/>
+    <ScrollView contentContainerStyle={{paddingBottom:100,padding:15,marginTop:15,}} showsVerticalScrollIndicator={false}>
+    <SendReceiveButton/> 
+    <Options isHome/>
+    <Misc/>
+    </ScrollView>
     </View>
   )
 }
